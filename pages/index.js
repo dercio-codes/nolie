fb69f029e7604d947feff0e644ef72e3d3c75467
Dercio <12derciomaduna@gmail.com>
2022-08-02 10:04:26 +0200

    Nolie

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import Option10 from "../components/navbar/option-10";
-import styles from "../styles/Home.module.css";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <>
-      <Option10 />
-    </>
+    <div style={{ width:"100%" , height:'100vh' , display:'flex' , alignItems:'center' , flexDirection:'column', justifyContent:'center'}}>
+    <img height={"250px"} src="https://img.icons8.com/color/344/worker-with-roadblock.png" alt="" />
+      <Typography fontSize={"32px"} fontWeight={"600"}>Website Under Construction</Typography>
+      <Typography color={"#222"}>We will be up and running soon.</Typography>
+    </div>
   );
 }
